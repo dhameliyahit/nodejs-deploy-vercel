26989e45d834a19c6a38b373d30695c73d1e2ab5
dhameliyahit <heetdhameliya59@gmail.com>
2024-09-08 21:55:41 +0530

    first commit && deploy

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const APP_PORT = 8080;
+const app = express();
+
+app.get("/",(req,res)=>{
+    res.end("<h1>Node+Express deploy On Vercel</h1>")
+});
+
+
+app.listen(APP_PORT,()=>{
+    console.log(`Server is Runnng on PORT no. ${APP_PORT}`);
+})
